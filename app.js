2f8db5a601e153acf8319bccabe9b20f1a412024
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-09 14:22:18 -0300

    made writeFile method asynchrnous

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-const http = require('http');
+const http = require("http");
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    const fs = require('fs');
+  const url = req.url;
+  const method = req.method;
+  const fs = require("fs");
 
-    if(url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body> <form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Send</button></form></body')
-        res.write('</html>');
-        return res.end();
-    }
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body> <form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Send</button></form></body'
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
-    if(url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
 
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split("=")[1]; 
-            fs.writeFileSync('message.txt', message);
-        });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
         res.statusCode = 302;
-        res.setHeader('Location', '/');
+        res.setHeader("Location", "/");
         return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my node.js server</h1></body>')
-    res.write('</html>');
-    res.end();
+      });
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my node.js server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
