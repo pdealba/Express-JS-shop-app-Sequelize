aef13c53a08467052abf255b80b64a3889326989
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-13 19:09:57 -0300

    added basic styling to html pages

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const path = require("path");
 
+const rootDir = require('../util/path');
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
