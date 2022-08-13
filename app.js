ed093d4c7ab3d2ddf9495684214afc159e31e914
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-13 18:58:55 -0300

    added 404 page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const path = require('path')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -12,7 +13,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found!</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
