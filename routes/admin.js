dd1d96e117b0ba9febf387f5ece84a884540e4f5
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-13 18:53:51 -0300

    creating and serving html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
 
+const path = require("path");
+
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add</button></form></html>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
