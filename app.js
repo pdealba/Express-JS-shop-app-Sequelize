fd608f190c6aa8dbbfee40b8b7cd750b2aa76322
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-16 12:39:53 -0300

    final commit before adding new express handlebars template engine

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use('/admin', admitData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
   res.status(404).render('404', {pageTitle: 'Page Not Found'});
 })
 
--- routes/shop.js
+++ routes/shop.js
@@ -7,6 +7,7 @@ const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 router.get("/", (req, res, next) => {
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
   const products = adminData.products
   res.render('shop', {prods: products, path: '/', pageTitle: 'Shop'})
 });
