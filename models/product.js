8b761d47214c8223520068436fd482ee30fc43c7
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-17 15:45:53 -0300

    improved code in model product.js file

--- models/product.js
+++ models/product.js
@@ -1,22 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "product.json"
+);
+
+const getsProductFromFiles = (cb) => {
+  fs.readFile(p, (err, data) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(data));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "product.json"
-    );
-    fs.readFile(p, (err, data) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(data);
-      }
+    getsProductFromFiles((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -25,16 +32,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "product.json"
-    );
-    fs.readFile(p, (err, data) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(data));
-    });
+    getsProductFromFiles(cb);
   }
 };
