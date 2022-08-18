3027bba82b0ef36ec8dd0d275eaec00c65431fe4
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-18 16:10:43 -0300

    fixed many mistakes in the directorys of the routes and added functionality for the product data to display

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,16 +1,5 @@
 const Product = require("../models/product");
 
-exports.getAddProduct = (req, res, next) => {
-  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("admin/add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-};
-
 exports.getProducts = (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   Product.fetchAll((products) => {
