039303ef85c66d46ce1446c9aac87b5c10807289
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-19 11:47:22 -0300

    added a unique ID for each product, and began extraction using params

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,6 +14,12 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  console.log(prodId);
+  res.redirect('/');
+}
+
 exports.getIndex = (req, res) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
--- models/product.js
+++ models/product.js
@@ -26,6 +26,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = Math.random();
     getsProductFromFiles((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
