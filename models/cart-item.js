0a99837d2eae334fce2e4b1dc3aa5edea2190b2b
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-25 14:05:16 -0300

    added cart, cart-item model

--- models/cart-item.js
+++ models/cart-item.js
@@ -2,13 +2,14 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../util/database");
 
-const cart = sequelize.define('cart', {
+const cartItem = sequelize.define('cartItem', {
   id: {
     type: Sequelize.INTEGER,
     allowNull: false,
     autoIncrement: true,
     primaryKey: true,
   },
+  quantity: Sequelize.INTEGER,
 });
 
-module.exports = cart;
+module.exports = cartItem;
