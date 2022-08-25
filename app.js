bfe27cc570f7ac3e5ca60276776b31a8009e1f26
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-08-25 11:02:26 -0300

    replacing SQL language, started implementation of sequelize

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const errorController = require("./controllers/error");
-
 const app = express();
-
 app.set("view engine", "ejs");
 
 const shopRoutes = require("./routes/shop");
 const admitRoutes = require("./routes/admin");
+const errorController = require("./controllers/error");
+
+const sequelize = require("./util/database");
 
 /*
 const expressHbs = require('express-handlebars')
@@ -30,4 +29,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql2 = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql2.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Paraiso22'
+const sequelize = new Sequelize("node-complete", "root", "Paraiso22", {
+  host: "localhost",
+  dialect: "mysql",
 });
 
-module.exports = pool.promise()
+module.exports = sequelize;
